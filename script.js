4732510c72459bebb27fae2918a81d44616ee57e
barezzali2 <barezrealy22@gmail.com>
2025-01-13 00:20:34 +0300

    going forward with coding!

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ async function addProducts() {
     const productName = document.getElementById('name').value;
     const productYear = document.getElementById('year').value;
     const productCountry = document.getElementById('country').value;
-    const submitBtn = document.getElementById('submit').value;
 
     const request = await fetch('http://localhost:3002/addProduct' , {
 
