163a574c29f6051bbe642b3e64bee5c002c45789
barezzali2 <barezrealy22@gmail.com>
2025-01-13 00:15:31 +0300

    going forward with coding!

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 const express = require('express')
 const app = express();
 const path = require('path')
-const port = 3000
+const port = 3002
 const { MongoClient } = require('mongodb')
 
 const uri = "mongodb://localhost:27017/products";
 const client = new MongoClient(uri);
 
-client.connect().then(() => {
-    console.log("Connected to MongoDB!");
+// client.connect().then(() => {
+//     console.log("Connected to MongoDB!");
 
-    const db = client.db("products");
-    // db.collection("drinks").insertOne({name: "Sprite", year: 2025, country: "UK"})
-    db.collection("drinks").findOne({country: "UK"})
-        .then((res) => console.log(res.name))
-        .catch((err) => console.error(err))
-        .then(() => console.log("A document is inserted successfully!"))
+//     const db = client.db("products");
+//     // db.collection("drinks").insertOne({name: "Sprite", year: 2025, country: "UK"})
+//     // Promise Chaining --> for fetching/getting data from DB
+//     db.collection("drinks").findOne({country: "UK"})
+//         .then((res) => console.log(res.name))
+//         .catch((err) => console.error(err))
+//         .then(() => console.log("A document is inserted successfully!"))
 
-}).catch((err) => console.log("Connection failed", err));
+// }).catch((err) => console.log("Connection failed", err));
 
 
-app.get("/hello", (req, res) => {
-    res.sendFile(path.join(__dirname, 'hello.html'));
+
+app.post("/addProduct", (req, res) => {
+    
+
 })
+
+// app.get("/hello", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'hello.html'));
+// })
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-function addProduct() {
+async function addProducts() {
     const productName = document.getElementById('name').value;
     const productYear = document.getElementById('year').value;
     const productCountry = document.getElementById('country').value;
     const submitBtn = document.getElementById('submit').value;
 
+    const request = await fetch('http://localhost:3002/addProduct' , {
 
+    })
 }
