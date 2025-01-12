97c078f848ef65ff3704ce52b811b50ad5e068f8
barezzali2 <barezrealy22@gmail.com>
2025-01-13 00:01:40 +0300

    A page is added to make a real-simple application

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function addProduct() {
+    const productName = document.getElementById('name').value;
+    const productYear = document.getElementById('year').value;
+    const productCountry = document.getElementById('country').value;
+    const submitBtn = document.getElementById('submit').value;
+
+
+}
