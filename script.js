2a1d2556cf0f9e7a51cff37295b8bb6b6643c4bf
barezzali2 <barezrealy22@gmail.com>
2025-01-14 13:30:23 +0300

    JS codes for the connection and backend

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-
 async function addProducts() {
-    const productName = document.getElementById('name').value;
-    const productYear = document.getElementById('year').value;
-    const productCountry = document.getElementById('country').value;
+    const productName = document.getElementById('name');
+    const productYear = document.getElementById('year');
+    const productCountry = document.getElementById('country');
 
-    const request = await fetch('http://localhost:3002/addProduct' , {
+    try {
+        const response = await fetch('http://localhost:3002/addDrinkProduct', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: productName.value,
+                year: parseInt(productYear.value),
+                country: productCountry.value,
+            }),
+        });
 
-    })
+        if(response.ok) {
+            console.log("Product is added successfully!");
+            productName.value =  productYear.value = productCountry.value = "";
+        } else {
+            const errorText = await response.text();
+            console.error("Failed to add product:", errorText);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("An error occurred: " + error.message);
+    }
 }
