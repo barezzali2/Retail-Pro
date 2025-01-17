2a1e32a843ff29cbe6a1fbae896a99e3549c1333
barezzali2 <barezrealy22@gmail.com>
2025-01-17 10:19:42 +0300

    Message confirmation is added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ async function addProducts() {
     const productName = document.getElementById('name');
     const productYear = document.getElementById('year');
     const productCountry = document.getElementById('country');
+    const message = document.getElementById('message');
 
     try {
         const response = await fetch('http://localhost:3002/addDrinkProduct', {
@@ -18,9 +19,12 @@ async function addProducts() {
 
         if(response.ok) {
             console.log("Product is added successfully!");
+            message.textContent = "The item " + `"${productName.value}"` + " is added successfully!";
             productName.value =  productYear.value = productCountry.value = "";
         } else {
             const errorText = await response.text();
+            message.textContent = "The item failed to add!";
+            message.style.color = "red";
             console.error("Failed to add product:", errorText);
         }
     } catch (error) {
